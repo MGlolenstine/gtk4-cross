208e5eaef307853868068fb0720ef3e7dbab221f
Milan Stastny <milan@acheta.games>
2021-11-03 09:22:49 +0100

    Use base image

--- gtk4-cross-meson/build.sh
+++ gtk4-cross-meson/build.sh
@@ -2,7 +2,7 @@
 source ~/.cargo/env
 export MINGW_PREFIX=/usr/x86_64-w64-mingw32/sys-root/mingw/
 
-x86_64-meson _build -Dwindows=true
+x86_64-meson _build -Dwindows=true --prefix=/usr
 meson compile -C _build
 
 mkdir -p package
