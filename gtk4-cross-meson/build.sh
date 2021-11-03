c61e1382726f7236998f63022b6c0d12f3503700
Milan Stastny <milan@acheta.games>
2021-11-03 12:49:07 +0100

    Fix paths

--- gtk4-cross-meson/build.sh
+++ gtk4-cross-meson/build.sh
@@ -2,8 +2,8 @@
 source ~/.cargo/env
 export MINGW_PREFIX=/usr/x86_64-w64-mingw32/sys-root/mingw/
 
-x86_64-meson _build -Dwindows=true --prefix=/usr
-meson compile -C _build
+x86_64-meson /tmp/_build -Dwindows=true --prefix=/usr
+meson compile -C /tmp/_build
 
 mkdir -p package
-cp _build/target/x86_64-pc-windows-gnu/release/*.exe package
+cp /tmp/_build/target/x86_64-pc-windows-gnu/release/*.exe package
