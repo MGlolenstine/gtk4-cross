dc1bce4f246ba35885de3664d0abe2a778614fba
MGlolenstine <mglolenstine@gmail.com>
2022-02-04 10:49:04 +0100

    Added schema compilation to the build script so that it works fine on Windows out of the box.

--- gtk4-cross-rust/build.sh
+++ gtk4-cross-rust/build.sh
@@ -3,4 +3,9 @@ source ~/.cargo/env
 cargo build --target=x86_64-pc-windows-gnu --release
 
 mkdir -p package
-cp target/x86_64-pc-windows-gnu/release/*.exe package
+cp target/x86_64-pc-windows-gnu/release/*.exe package/
+
+# Handle the glib schema compilation as well
+glib-compile-schemas /usr/x86_64-w64-mingw32/sys-root/mingw/share/glib-2.0/schemas/
+mkdir -p package/share/glib-2.0/shemas/
+cp /usr/x86_64-w64-mingw32/sys-root/mingw/share/glib-2.0/schemas/gschemas.compiled package/share/glib-2.0/shemas/
